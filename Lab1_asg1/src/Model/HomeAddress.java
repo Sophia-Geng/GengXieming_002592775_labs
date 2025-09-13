cfb1bcccd933abd7531818b90bdeeb509ffe48f6
Sophia-Geng <geng.xie@northesatern.edu>
2025-09-13 19:15:32 -0400

    Add Assignment

---
+++ Lab1_asg1/src/Model/HomeAddress.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author DELL
+ */
+public class HomeAddress {
+    String name;
+    String street;
+    String unit;
+    String city;
+    String zipcode;
+}
